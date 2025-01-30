1cbe4e1c33d9feec3b8a245523bc677f7a7b6346
Saul <saultoribio7@gmail.com>
2025-01-30 17:55:15 -0500

    ?

--- CAs/CA_2/adventure.py
+++ CAs/CA_2/adventure.py
@@ -15,8 +15,8 @@ MONSTER_HEALTH = 75
 
 # Introduction
 INTRO = (
-    f"After having one of the most vivid nightmares of your life, "
-    f"awoke in a cold sweat. Rather than awakening in your bed, you found yourself in a crop circle,\n"
+    f"After having one of the most vivid nightmares of your life, awoke "
+    f"in a cold sweat. Rather than awakening in your bed, you found yourself in a crop circle,\n"
     f"surrounded by towering walls of wheat, with no earthly idea of how you had gotten there. "
     f"Despite feeling disoriented and nauseated,\nyou noticed a path forward—your \"way out.\" "
     f"After walking for a while, you come across a fork in the path."
@@ -24,8 +24,8 @@ INTRO = (
 print(INTRO + "\n")
 
 # Path Choice
-# Again, I just used a switch-case statement to decide whether or not the program will randomly choose left or right.
-# Once the program chooses, it will play out that case scenario.
+# Again, I just used a switch-case statement to decide whether or not the program will
+# randomly choose left or right. Once the program chooses, it will play out that case scenario.
 match random.choice(["left", "right"]):
     case "left":
         print("You decided to walk on the left path.\n")
@@ -52,14 +52,16 @@ print("You encountered a monster!\n")
 HAS_TREASURE = random.choice([True, False])
 
 # Combat Encounter
-# Here, the program will run a loop of you fighting a monster, which will end when PLAYER_HEALTH or MONSTER_HEALTH == 0.
+# Here, the program will run a loop of you fighting a monster,
+# which will end when PLAYER_HEALTH or MONSTER_HEALTH == 0.
 LOOP = True
 while LOOP is True:
     # The player attacks first, doing a flat 15 damage.
     print("You strike the monster for 15 damage!\n")
     MONSTER_HEALTH -= 15
 
-    # Then, if PLAYER_HEALTH > 0, the monster will attack, either doing 10 or 20 damage if it lands a crit.
+    # Then, if PLAYER_HEALTH > 0, the monster will attack,
+    # either doing 10 or 20 damage if it lands a crit.
     if PLAYER_HEALTH > 0:
         CRIT = int(random.random() * 2)
         match CRIT:
@@ -70,7 +72,8 @@ while LOOP is True:
                 print("The monster lands a critical hit for 20 damage!\n")
                 PLAYER_HEALTH -= 20
 
-    # If PLAYER_HEALTH <= 0, then it will print "Game Over!", while if MONSTER_HEALTH <= 0, then it will run the Treasure Check.
+    # If PLAYER_HEALTH <= 0, then it will print "Game Over!",
+    # while if MONSTER_HEALTH <= 0, then it will run the Treasure Check.
     if PLAYER_HEALTH <= 0:
         print("Game Over!\n")
         LOOP = False
